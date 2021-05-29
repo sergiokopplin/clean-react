d5d639ef6b1d0f0aaaa15204174b1c7322a1261e
Sergio Kopplin <sergiokopplin@gmail.com>
2021-05-29 18:11:25 -0300

    feat: ensure Login will not load spinner and error on start

--- src/presentation/components/form-status/form-status.tsx
+++ src/presentation/components/form-status/form-status.tsx
@@ -1,16 +1,19 @@
-import { FC } from 'react';
+import { FC, useContext } from 'react';
 
 import { Spinner } from 'presentation/components/spinner/spinner';
+import { FormContext } from 'presentation/contexts/form/form-context';
 
 import Styles from './form-status-styles.module.scss';
 
 export interface FormStatusProps {}
 
 export const FormStatus: FC<FormStatusProps> = () => {
+  const { isLoading, errorMessage } = useContext(FormContext);
+
   return (
-    <div className={Styles.errorWrap}>
-      <Spinner className={Styles.spinner} />
-      <span className={Styles.error}>Erro</span>
+    <div data-testid="error-wrap" className={Styles.errorWrap}>
+      {isLoading && <Spinner className={Styles.spinner} />}
+      {errorMessage && <span className={Styles.error}>Erro</span>}
     </div>
   );
 };
--- src/presentation/pages/login/login.tsx
+++ src/presentation/pages/login/login.tsx
@@ -1,28 +1,45 @@
-import { FC } from 'react';
+import { FC, useState } from 'react';
 
 import { Footer } from 'presentation/components/footer/footer';
 import { FormStatus } from 'presentation/components/form-status/form-status';
 import { Input } from 'presentation/components/input/input';
 import { LoginHeader } from 'presentation/components/login-header/login-header';
+import { FormContext } from 'presentation/contexts/form/form-context';
 
 import css from './login-styles.module.scss';
 
 export interface LoginProps {}
 
+type LoginState = {
+  isLoading: boolean;
+  errorMessage: string;
+};
+
 export const Login: FC<LoginProps> = () => {
+  const [state] = useState<LoginState>({
+    isLoading: false,
+    errorMessage: '',
+  });
+
   return (
     <div className={css.login}>
       <LoginHeader />
-      <form className={css.form}>
-        <h2>Login</h2>
-        <Input type="email" name="email" placeholder="Digite seu e-mail" />
-        <Input type="password" name="password" placeholder="Digite sua senha" />
-        <button className={css.submit} type="submit">
-          Entrar
-        </button>
-        <span className={css.link}>Criar conta</span>
-        <FormStatus />
-      </form>
+      <FormContext.Provider value={state}>
+        <form className={css.form}>
+          <h2>Login</h2>
+          <Input type="email" name="email" placeholder="Digite seu e-mail" />
+          <Input
+            type="password"
+            name="password"
+            placeholder="Digite sua senha"
+          />
+          <button className={css.submit} type="submit">
+            Entrar
+          </button>
+          <span className={css.link}>Criar conta</span>
+          <FormStatus />
+        </form>
+      </FormContext.Provider>
       <Footer />
     </div>
   );
